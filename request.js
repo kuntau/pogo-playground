9d01d3f80ee9918bce3ab5b708df6efef1f579bb
Kuntau <kuntau17@gmail.com>
2016-09-19 19:09:24 +0800

    now we can loop the files

--- request.js
+++ request.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var os = require('os');
 var r = require('request');
 
 var fpm_lat = 2.9352298769971,
@@ -25,14 +26,49 @@ function fpm_getPokemon(lat, lng) {
           fpm_getPokemon(lat, lng);
         }, 500);
       }
-      else {
-        // fs.write
-        console.log(data);
-      }
+
+      // proceed if no error
+      console.log(data.result);
+      return data.result;
 
     }
   })
 }
 
+function readCoord(file) {
+  fs.readFile(file, function (err, data) {
+    if (err) console.log(err);
+    var fileCoords = JSON.parse(data);
+    var pokeCoords = [];
+    var tempCoord;
 
-fpm_getPokemon(1,2);
+    fileCoords.forEach(function (loc) {
+      setTimeout(function () {
+        tempCoord = fpm_getPokemon(loc.lat, loc.lng);
+        pokeCoords.push(tempCoord);
+      },1000)
+
+    })
+    appendFile('results.json', JSON.stringify(pokeCoords, null, 2) + os.EOL);
+  })
+}
+
+function appendFile(file, data) {
+  fs.appendFile(file, data, function (err) {
+    if (err) console.log("appendFile ", err)
+    console.log("appendFile ", file, data);
+  })
+}
+
+function writeFile(file, data) {
+  fs.writeFile(file, data, function (err) {
+    if (err) console.log("writeFile ", err)
+    console.log("writeFile ", file, data);
+  })
+}
+
+// fpm_getPokemon(1,2);
+// readCoord('POGO_KL_POI.json');
+readCoord('POI.json');
+// writeFile('results.json', '')
+// appendFile('results.json', 'LOREM IPSUM')
